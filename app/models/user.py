60023bfb722fe839f90e9de4ee6dd0b9f8ba5ade
Collins Isiwu <collinscisiwu@outlook.com>
2024-07-10 13:09:33 +0100

    debugged User model

--- app/models/user.py
+++ app/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     is_active = db.Column(db.Boolean(), default=True)
     is_admin = db.Column(db.Boolean(), default=False)
     active_loan = db.Column(db.Boolean(), default=False)
--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
+
 pip install -r requirements.txt
 
 flask db init
